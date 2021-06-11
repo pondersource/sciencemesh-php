83a64ac92b6037204a8e4056f3ccfd4a22e934fa
Michiel de Jong <michiel@unhosted.org>
2021-06-11 16:40:46 +0200

    trying to get docker images working

--- build.sh
+++ build.sh
@@ -3,4 +3,7 @@
 rm -rf docker/nc/nc-sciencemesh
 cp -r nc-sciencemesh docker/nc/
 docker build -t nc docker/nc/
+
+rm -rf docker/revad/reva-storage-nextcloud
+cp -r reva-storage-nextcloud docker/revad/
 docker build -t revad docker/revad/
--- run.sh
+++ run.sh
@@ -1,41 +1,18 @@
 #!/bin/bash
 set -e
 
-function setup {
-  docker network create testnet
-  # docker build -t nc https://github.com/pdsinterop/solid-nextcloud/
-  # docker build -t revad ./revad/
-}
+docker network create testnet
+docker run -d --name nc --network=testnet nc
+until docker run --rm --network=testnet nc curl -kI https://nc 2> /dev/null > /dev/null
+do
+  echo Waiting for nc to start, this can take up to a minute ...
+  docker ps -a
+  docker logs nc
+  sleep 1
+done
+docker logs nc
+echo Running init script for Nextcloud...
+docker exec -u www-data -it -e SERVER_ROOT=https://nc nc sh /init.sh
+docker exec -u root -it $1 service apache2 reload
 
-function teardown {
-  docker stop `docker ps --filter network=testnet -q`
-  docker rm `docker ps --filter network=testnet -qa`
-  docker network remove testnet
-}
-
-function startNextcloud {
-  docker run -d --name $1 --network=testnet nc
-  until docker run --rm --network=testnet nc curl -kI https://$1 2> /dev/null > /dev/null
-  do
-    echo Waiting for $1 to start, this can take up to a minute ...
-    docker ps -a
-    docker logs $1
-    sleep 1
-  done
-
-  docker logs $1
-  echo Running init script for Nextcloud $1 ...
-  docker exec -u www-data -it -e SERVER_ROOT=https://$1 $1 sh /init.sh
-  docker exec -u root -it $1 service apache2 reload
-}
-
-function startRevad {
-  docker run -d --name $1 --network=testnet revad
-}
-
-# ...
-teardown || true
-setup
-startNextcloud nextcloud
-startRevad revad
-teardown
+docker run -d --name revad --network=testnet revad
