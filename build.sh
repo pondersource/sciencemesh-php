17183076811da3b566bbd0daf0de53d4ba6c848b
Michiel de Jong <michiel@unhosted.org>
2021-06-16 15:28:17 +0200

    move directories around

--- build.sh
+++ build.sh
@@ -1,9 +1,4 @@
 #!/bin/bash
 
-rm -rf docker/nc/nc-sciencemesh
-cp -r nc-sciencemesh docker/nc/
 docker build -t nc docker/nc/
-
-rm -rf docker/revad/reva-storage-nextcloud
-cp -r reva-storage-nextcloud docker/revad/
 docker build -t revad docker/revad/
