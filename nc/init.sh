ad60482ca79bad781af7f1876dd003ab472d64b0
Michiel de Jong <michiel@unhosted.org>
2021-09-06 14:31:28 +0200

    Einstein instead of Alice

--- nc/init.sh
+++ nc/init.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 export PHP_MEMORY_LIMIT="512M"
-php console.php maintenance:install --admin-user alice --admin-pass alice123
+php console.php maintenance:install --admin-user einstein --admin-pass relativity
 php console.php status
 php console.php app:enable sciencemesh
 echo configured
